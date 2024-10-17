30b125a982109198302ba4841b464ff0b94e3aeb
Ivan <i.i.boltachev@yandex.ru>
2024-10-17 18:32:28 +0300

    add mongodb

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Мое Flask приложение в контейнере Docker !!!'
+client = MongoClient('mongodb', 27017)
+db = client.tododb
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route('/')
+def todo():
+    _items = db.tododb.find()
+    items = [item for item in _items]
+    return render_template('todo.html', items=items)
+
+@app.route('/new', methods=['POST'])
+def new():
+    item_doc = {
+    'task': request.form['task'],
+    'description': request.form['description']
+    }
+    db.tododb.insert_one(item_doc)
+    return redirect(url_for('todo'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
